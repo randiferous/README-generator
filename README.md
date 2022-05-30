2c620fa9584fdc45f446f53de774fba56e105b6d
lotus <daniel.younghwan.lee@gmail.com>
2022-05-30 13:35:18 -0400

    restructure folder

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Professional README Generator Starter Code
+# Professional README Generator Challenge
+The purpose of this challenge is to introduce us to Node.js and the world of back-end development. We are required to create a ReadMe file through a code that takes input data from the command line and outputs it in the form of a ReadMe.
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+- First major hurdle was figuring out how to get started. I felt like I knew nothing all over again. Rereading through the module and referring to the module code certainly helped here.
+- I must give credit to GitHub user jpd61 for providing me a very similar project as a reference.  I studied his code to understand why and how certain syntax works, and applied them to my assignment wherever I deemed necessary. I would only apply code after I fully understood the meaning of its application.
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,69 @@
+
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(projectInfo) { 
+  if (projectInfo.license === 'N/A') {
+    return '';
+  }
+
+  return `
+  ![badge](https://img.shields.io/badge/license-${projectInfo.license}-blue)
+  `
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(projectInfo) {
+  if (projectInfo.license === 'N/A') {
+    return '';
+  }
+
+  return `
+  This application is covered under ${projectInfo.license}
+  `
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(projectInfo) {
+  return `
+  # ${projectInfo.title}
+  ${renderLicenseBadge(projectInfo)}
+
+  ## Description
+  ${projectInfo.description}
+
+  ## Table of Contents
+  ** [Installation](#installation)
+  ** [Usage](#usage)
+  ** [License](#license)
+  ** [Contributing](#contributing)
+  ** [Tests](#tests)
+  ** [Questions](#questions)
+
+  ## Installation
+  ${projectInfo.installation}
+
+  ## Usage
+  ${projectInfo.usage}
+
+  ## License
+  ${renderLicenseSection(projectInfo)}
+
+  ## Contributing
+  ${projectInfo.contributing}
+
+  ## Tests
+  ${projectInfo.test}
+
+  ## Questions
+  You can find me on GitHub at [${projectInfo.username}](https://github.com/${projectInfo.username})
+  If any have any questions, reach me at:${projectInfo.email}
+  `;
+}
+
+module.exports = generateMarkdown;
